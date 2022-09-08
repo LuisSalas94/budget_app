330342528114f3c027701fba39ce6f407a579d0c
Fernando Salas <luisfernandosalasg@gmail.com>
2022-09-08 00:11:59 -0500

    Update Roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+devise_for :users
   
+ devise_scope :user do
+    authenticated do
+      root to: 'groups#index', as: 'user'
+    end
+
+    unauthenticated do
+      #root to: 'menus#index', as: 'unauthenticated_user_root'
+    end
+
+    get '/sign_out', to: 'devise/sessions#destroy'
+  end
+    
   resources :groups do
     resources :expenses
   end
-  root :to => 'groups#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
