54093548f86b61b716d3dc132f8827abe230ed5c
Fernando Salas <luisfernandosalasg@gmail.com>
2022-09-10 00:34:33 -0500

    Fix linters

--- app/models/group_expense.rb
+++ app/models/group_expense.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class GroupExpense < ApplicationRecord
   belongs_to :expense
   belongs_to :group
--- spec/views/expense_new_spec.rb
+++ spec/views/expense_new_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 RSpec.describe 'Expense', type: :system do
   describe 'New' do
