732f4c843beb526a982935e165afd72b5ef8d5c0
Fernando Salas <luisfernandosalasg@gmail.com>
2022-09-10 02:58:00 -0500

    Add required changes

--- spec/views/group_index_spec.rb
+++ spec/views/group_index_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 RSpec.describe 'Group', type: :system do
   describe 'Index' do
     before(:each) do
-      User.create(name: 'Test', email: 'b@b', password: '123456')
+      User.create(name: 'Test', email: 'test@gmail.com', password: '123456')
       visit new_user_session_path
-      fill_in 'Email', with: 'b@b'
+      fill_in 'Email', with: 'test@gmail.com'
       fill_in 'Password', with: '123456'
       click_button 'Log in'
     end
-
-    it 'should show groups' do
+  
+    it 'It should show groups name' do
       expect(page).to have_content('TRANSACTIONS')
     end
 
-    it 'show show add category button' do
+    it 'It show add category button name' do
       expect(page).to have_content('ADD CATEGORY')
     end
 
